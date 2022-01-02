2713b22f1f4c52d00cc82ec2eebd1fc2ad24dc32
Emmanuel Ezejiobi <eze6266@gmail.com>
2022-01-02 17:31:39 +0100

    This is a simple dice app

---
+++ android/app/src/main/kotlin/com/example/dicey_dice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dicey_dice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicey'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int rightDiceNumber = 1;
+  int leftDiceNumber = 1;
+
+  void dicePageChange() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                dicePageChange();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                dicePageChange();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
